771e4f57f782ad1528c2276690d83e63a8b80ea7
Joel Poulin <joel@voilab.ch>
2016-10-12 16:51:29 +0200

    - object management in localStorage

--- util/storage.factory.js
+++ util/storage.factory.js
@@ -6,61 +6,66 @@ define([
 ], function (app, lodash) {
     'use strict';
     return app.factory('store', ['$window', function ($window) {
-            var retrieveValue = function (str) {
+        var retrieveValue = function (str) {
+            try {
+                return JSON.parse(str);
+            } catch (e) {
+                return str;
+            }
+        };
+        return {
+            setLocal: function (key, value) {
                 try {
-                    return JSON.parse(str);
-                } catch (e) {
-                    return str;
+                    if ($window.Storage) {
+                        if (lodash.isObject(value)) {
+                            value = JSON.stringify(value);
+                        }
+                        $window.localStorage.setItem(key, value);
+                        return true;
+                    }
+                    return false;
+                } catch (error) {
+                    console.error(error, error.message);
                 }
-            };
-            return {
-                setLocal: function (key, value) {
-                    try {
-                        if ($window.Storage) {
-                            $window.localStorage.setItem(key, value);
-                            return true;
-                        }
-                        return false;
-                    } catch (error) {
-                        console.error(error, error.message);
+            },
+            getLocal: function (key) {
+                var result;
+                try {
+                    if ($window.Storage) {
+                        result = $window.localStorage.getItem(key);
+                        return retrieveValue(result);
                     }
-                },
-                getLocal: function (key) {
-                    try {
-                        if ($window.Storage) {
-                            return $window.localStorage.getItem(key);
-                        }
-                        return false;
-                    } catch (error) {
-                        console.error(error, error.message);
-                    }
-                },
-                setSession: function (key, value) {
-                    try {
-                        if ($window.Storage) {
-                            if (lodash.isObject(value)) {
-                                value = JSON.stringify(value);
-                            }
-                            $window.sessionStorage.setItem(key, value);
-                            return true;
-                        }
-                        return false;
-                    } catch (error) {
-                        console.error(error, error.message);
-                    }
-                },
-                getSession: function (key) {
-                    var result;
-                    try {
-                        if ($window.Storage) {
-                            result = $window.sessionStorage.getItem(key);
-                            return retrieveValue(result);
-                        }
-                        return false;
-                    } catch (error) {
-                        console.error(error, error.message);
-                    }
+                    return false;
+                } catch (error) {
+                    console.error(error, error.message);
                 }
-            };
-        }]);
+            },
+            setSession: function (key, value) {
+                try {
+                    if ($window.Storage) {
+                        if (lodash.isObject(value)) {
+                            value = JSON.stringify(value);
+                        }
+                        $window.sessionStorage.setItem(key, value);
+                        return true;
+                    }
+                    return false;
+                } catch (error) {
+                    console.error(error, error.message);
+                }
+            },
+            getSession: function (key) {
+                var result;
+                try {
+                    if ($window.Storage) {
+                        result = $window.sessionStorage.getItem(key);
+                        return retrieveValue(result);
+                    }
+                    return false;
+                } catch (error) {
+                    console.error(error, error.message);
+                }
+            }
+        };
+    }]);
 });
